08efb63ceb65fe98645466b24e4361950f889bd7
Abdullah Bin Rashid <abdullahbinrashid2005abr@gmail.com>
2025-11-21 21:16:57 +0500

    JavaFX Folder Structure

--- src/main/java/com/DullPointers/Main.java
+++ src/main/java/com/DullPointers/Main.java
@@ -1,3 +1,5 @@
+package com.DullPointers;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Java!");
---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module com.DullPointers {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    exports com.DullPointers;
+}
